b7a7c50db2fd05e78be48705179edc5fb22132ab
21608 <oka00027@gmail.com>
2023-08-25 22:02:08 +0900

    enumの基本的な使い方を確認

---
+++ basic/HowToUseEnum.java
@@ -0,0 +1,17 @@
+package basic;
+
+public enum HowToUseEnum {
+    JANUARY("1月") {
+        public String oneWord(int param) { return param + "度" + " cold"; }
+    },
+    JULY("7月") {
+        public String oneWord(int param) { return param + "度" + " hot"; }
+    };
+
+    private final String month;
+    HowToUseEnum(String month) {this.month = month;}
+
+    @Override public String toString() { return month; }
+
+    public abstract String oneWord(int param);
+}
---
+++ basic/Main.java
@@ -0,0 +1,8 @@
+package basic;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(HowToUseEnum.JANUARY.toString() + ":" + HowToUseEnum.JANUARY.oneWord(2));
+        System.out.println(HowToUseEnum.JULY.toString() + ":" + HowToUseEnum.JULY.oneWord(37));
+    }
+}
