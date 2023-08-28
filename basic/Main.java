4a057215086bd3992ebd188960ea88d2d5994cc8
21608 <oka00027@gmail.com>
2023-08-28 23:13:10 +0900

    EnumSetの使い方確認

--- basic/Main.java
+++ basic/Main.java
@@ -1,5 +1,8 @@
 package basic;
 
+import java.util.EnumSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println(HowToUseEnum.JANUARY.toString() + ":" + HowToUseEnum.JANUARY.setTemperature(2));
@@ -8,5 +11,11 @@ public class Main {
         System.out.println("APPLE:" + Ordinal.APPLE.numberOfFruits());
         System.out.println("ORANGE:" + Ordinal.ORANGE.numberOfFruits());
         System.out.println("BANANA:" + Ordinal.BANANA.numberOfFruits());
+
+        Set<HowToUseEnum> enumSets = EnumSet.of(HowToUseEnum.JANUARY, HowToUseEnum.JULY); 
+
+        System.out.println("enumSets_size:" + enumSets.size());
+        System.out.println("contain_JANUARY? " + enumSets.contains(HowToUseEnum.JANUARY));
+        System.out.println("contain_JULY? " + enumSets.contains(HowToUseEnum.JULY));
     }
 }
